19f7139cf9174220d478df0a652a1f51e2b0a356
jodiy <2381057@unai.edu>
2024-11-08 14:51:00 +0700

    handson8

---
+++ entities/TodoList.java
@@ -0,0 +1,27 @@
+package entities;
+
+public class TodoList {
+    private String todo;
+    private int id;
+
+    public TodoList() {
+        this.todo = todo;
+        this.id = id;
+    }
+
+    public String getTodo() {
+        return todo;
+    }
+
+    public void setTodo(String todo) {
+        this.todo = todo;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+}
